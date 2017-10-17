2743542294661e8c0e7ecda5ca07d09f3045d0be
pwrliang <pwrliang@gmail.com>
2017-10-17 10:26:39 +0800

    update script

--- bin/run.sh
+++ bin/run.sh
@@ -8,24 +8,30 @@ function run(){
     ${BIN}/kill-all.sh ${MACHINES}
 
     mpjrun.sh -Xmx28G \
-    -machinesfile ${MACHINES} -np $((MACHINES_NUM+1)) -dev native \
+    -machinesfile ${MACHINES} -np $((MACHINES_NUM+1)) -dev niodev \
     -Dsocialite.output.dir=${SOCIALITE_PREFIX}/gen \
     -Dsocialite.port=50100 \
     -Dsocialite.master=${MASTER_HOST} \
     -Dlog4j.configuration=file:${SOCIALITE_PREFIX}/conf/log4j.properties \
     -cp ${CODE_CLASSPATH}:${JAR_PATH} \
     socialite.async.Entry ${SOCIALITE_PREFIX}/$1
-#    mpirun -machinefile ${MACHINES} -np $((MACHINES_NUM+1)) \
-#    java -Djava.library.path=$MPJ_HOME/lib \
-#    -Dsocialite.output.dir=${SOCIALITE_PREFIX}/gen \
-#    -Dsocialite.port=50100 \
-#    -Dsocialite.master=${MASTER_HOST} \
-#    -Dlog4j.configuration=file:${SOCIALITE_PREFIX}/conf/log4j.properties \
-#    -cp $MPJ_HOME/lib/mpj.jar:${CODE_CLASSPATH}:${JAR_PATH} \
-#    socialite.async.Entry \
-#    0 0 native ${SOCIALITE_PREFIX}/$1
 }
 
+function run_with_mpi(){
+    ${BIN}/kill-all.sh ${MACHINES}
+
+    mpirun -machinefile ${MACHINES} -np $((MACHINES_NUM+1)) \
+        --mca btl_tcp_if_include eth0 \
+        -mca btl ^openib \
+        java -Djava.library.path=$MPJ_HOME/lib \
+        -Dsocialite.output.dir=${SOCIALITE_PREFIX}/gen \
+        -Dsocialite.port=50100 \
+        -Dsocialite.master=${MASTER_HOST} \
+        -Dlog4j.configuration=file:${SOCIALITE_PREFIX}/conf/log4j.properties \
+        -cp $MPJ_HOME/lib/mpj.jar:${CODE_CLASSPATH}:${JAR_PATH} \
+        socialite.async.Entry \
+        0 0 native ${SOCIALITE_PREFIX}/$1
+}
 
 
 if [ "$#" == "1" ]; then
@@ -53,7 +59,7 @@ if [ "$#" == "1" ]; then
         echo "please specify -copy-jar or -copy-classes"
         exit 1
     fi
-elif [ "$#" == "2" ]; then
+elif [ "$#" == "2" ] || [ "$#" == "3" ]; then
     if [ "$1" == "-run" ]; then
         CODE_CLASSPATH=${SOCIALITE_PREFIX}/classes/socialite.jar
 
@@ -63,7 +69,12 @@ elif [ "$#" == "2" ]; then
         echo "please specify [-run/-debug] [Datalog_Program]"
         exit 1
     fi
-    run $2
+
+    if [ "$#" == "3" ] && [ "$2" == "-native" ]; then
+        run_with_mpi $2
+    else
+        run $2
+    fi
 else
     echo "please specify [-copy-jar/-copy-classes] or [-run/-debug] [Datalog_Program]"
     exit 1
