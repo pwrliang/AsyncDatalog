0e8f3a5eb7d0c31f02a732028eb4170c15392e7e
pwrliang <pwrliang@gmail.com>
2017-10-28 21:34:32 +0800

    update mpj

--- src/socialite/async/Entry.java
+++ src/socialite/async/Entry.java
@@ -14,6 +14,7 @@ import socialite.engine.Config;
 import socialite.util.SociaLiteException;
 
 import java.lang.reflect.Field;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Entry {
@@ -28,6 +29,8 @@ public class Entry {
             sysPathsField.setAccessible(true);
             sysPathsField.set(null, null);
 
+            Random random = new Random();
+            Thread.sleep(random.nextInt(1000));
             MPI.Init(args);
             int machineNum = MPI.COMM_WORLD.Size();
             int machineId = MPI.COMM_WORLD.Rank();
