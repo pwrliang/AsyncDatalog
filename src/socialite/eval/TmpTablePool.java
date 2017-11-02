3a81ba51e1c3f5bf89ced234e076667fc8c177aa
pwrliang <pwrliang@gmail.com>
2017-11-02 15:12:58 +0800

    update preprocess

--- src/socialite/eval/TmpTablePool.java
+++ src/socialite/eval/TmpTablePool.java
@@ -263,6 +263,7 @@ public class TmpTablePool {
 			allocKB.addAndGet((t.totalAllocSize()+1023)/1024);
 			return t;
 		} catch (Exception e) {
+			e.printStackTrace();
 			throw new SociaLiteException(e);
 		}
 	}	
